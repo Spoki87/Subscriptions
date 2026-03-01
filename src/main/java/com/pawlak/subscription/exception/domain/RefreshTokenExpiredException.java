c337f70b3b9faab9cc35417e517e96fb878bb2e1
Patryk Pawlak <patrykpawlak6789@gmail.com>
2026-03-01 20:41:56 +0100

    fix:refresh token exception handler

--- src/main/java/com/pawlak/subscription/exception/domain/RefreshTokenExpiredException.java
+++ src/main/java/com/pawlak/subscription/exception/domain/RefreshTokenExpiredException.java
@@ -1,9 +1,10 @@
 package com.pawlak.subscription.exception.domain;
 
 import com.pawlak.subscription.exception.base.BusinessException;
+import org.springframework.http.HttpStatus;
 
 public class RefreshTokenExpiredException extends BusinessException {
     public RefreshTokenExpiredException() {
-        super("Refresh token expired", null);
+        super("Refresh token expired", HttpStatus.BAD_REQUEST);
     }
 }
